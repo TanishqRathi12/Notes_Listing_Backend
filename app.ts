4b808c70e8c0895cabf1416c7cadc34544864d53
TanishqRathi12 <rathitanishq2022@gmail.com>
2024-12-17 09:19:16 +0530

    cors issue fixed

--- app.ts
+++ app.ts
@@ -11,7 +11,13 @@ const app = express();
 dotenv.config();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+    {
+        origin: '*',
+        methods: ['GET', 'POST', 'DELETE', 'PUT'],
+        allowedHeaders: ['Content-Type', 'Authorization']
+    }
+));
 
 app.use('/auth',AuthRouter);
 app.use('/otp',OtpRouter);
